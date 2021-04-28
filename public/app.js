12b0943fc797560ce363cc01389c0e88d5ae0a70
2e12 <gabrielbeutler@gmail.com>
2021-04-28 09:32:29 +0200

    Load trigger zones

--- public/app.js
+++ public/app.js
@@ -1,12 +1,25 @@
 let facts = {}
 
-window.onload = () => {
+window.onload = function () {
     fetch('./facts.json')
     .then(response => response.json())
     .then( (data) => {
         facts = data
-        showFact('car')
+        hideFact()
+        setTriggers()
     })
+}
+
+function setTriggers() {
+    let a = document.getElementById("background")
+    let svgobj = a.contentDocument;
+    for (let [key, value] of Object.entries(facts)) {
+        obj = svgobj.getElementById('trigger-' + key)
+        obj.style.cursor = "Pointer"
+        obj.onclick = () => {
+            showFact(key)
+        }
+    }
 }
 
 function showFact(name) {
